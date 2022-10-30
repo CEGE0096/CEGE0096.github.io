4ee9a4a9f5ed30d6785cf42c35fba5fdee3a6453
CEGE0096 <dannykim153@gmail.com>
2022-10-30 14:36:06 +0000

    Update from https://github.com/CEGE0096/cw1-autograde-helper/commit/36c19fc14de073b1a203bd1f1932234cdfdaf112

---
+++ test_are_line_segments_orthogonal_sample.py
@@ -0,0 +1,24 @@
+import math
+import pickle
+
+from geometry import are_line_segments_orthogonal
+
+
+def read(input_file='are_line_segments_orthogonal_sample'):
+    with open(f'./tests/data/{input_file}.pkl', 'rb') as f:
+        rows = pickle.load(f)
+    return rows
+
+
+def test_are_line_segments_orthogonal_sample():
+    rows = read()
+    for row in rows:
+        args, res = row[:-1], row[-1]
+        res_hat = are_line_segments_orthogonal(*args)
+        if res_hat is None or res is None:
+            assert res_hat == res, args
+        elif type(res) is tuple:
+            for r, r_hat in zip(res, res_hat):
+                assert math.isnan(r) and math.isnan(r_hat) or abs(r_hat - r) <= 0.0001, args
+        else:
+            assert math.isnan(res) and math.isnan(res_hat) or abs(res_hat - res) <= 0.0001, args
---
+++ test_is_line_segment_horizontal_sample.py
@@ -0,0 +1,24 @@
+import math
+import pickle
+
+from geometry import is_line_segment_horizontal
+
+
+def read(input_file='is_line_segment_horizontal_sample'):
+    with open(f'./tests/data/{input_file}.pkl', 'rb') as f:
+        rows = pickle.load(f)
+    return rows
+
+
+def test_is_line_segment_horizontal_sample():
+    rows = read()
+    for row in rows:
+        args, res = row[:-1], row[-1]
+        res_hat = is_line_segment_horizontal(*args)
+        if res_hat is None or res is None:
+            assert res_hat == res, args
+        elif type(res) is tuple:
+            for r, r_hat in zip(res, res_hat):
+                assert math.isnan(r) and math.isnan(r_hat) or abs(r_hat - r) <= 0.0001, args
+        else:
+            assert math.isnan(res) and math.isnan(res_hat) or abs(res_hat - res) <= 0.0001, args
